117da6fff65cc88e8db88f28b0ebd1965c6bb6c1
Michael Dalton <modintexas@gmail.com>
2018-08-14 16:57:20 -0700

    ResponsiveTable component and it's relevant tests. Alot of relevant testing tools/webpack configs were added at 5f05cf126bcfa739170b6df46ec9be0fd9ca8940.

--- src/client/components/ResponsiveTable.jsx
+++ src/client/components/ResponsiveTable.jsx
@@ -1,7 +1,73 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const ResponsiveTable = props => {
-  return <div>ResponsiveTable</div>;
+const validColumn = col => {
+  return col.hasOwnProperty("hideable") && col.hasOwnProperty("title");
+};
+
+let ResponsiveTable = props => {
+  return (
+    <table>
+      {props.hasOwnProperty("columns") &&
+        props.hasOwnProperty("row_data") && (
+          <tbody>
+            <tr>
+              {/* Loop over columns and render if valid */}
+              {props.columns.map((col, idx) => {
+                if (validColumn(col))
+                  return (
+                    <th key={idx} className={col.hideable ? "hideable" : ""}>
+                      {col.title}
+                    </th>
+                  );
+              })}
+            </tr>
+            {/* Loop over row data and for each valid column pull out it's 'key' from
+                 said row data object
+              */}
+            {props.row_data.map((row, idx) => {
+              return (
+                <tr key={idx}>
+                  {props.columns.map((col, idx) => {
+                    if (validColumn(col))
+                      return (
+                        <td
+                          key={idx}
+                          className={col.hideable ? "hideable" : ""}
+                        >
+                          {!isNaN(row[col.key])
+                            ? Number.parseFloat(row[col.key]).toFixed(3)
+                            : row[col.key]}
+                        </td>
+                      );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        )}
+    </table>
+  );
+};
+
+// Proptypes here are use-case specific for the sake of terseness
+ResponsiveTable.propTypes = {
+  columns: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      hideable: PropTypes.bool.isRequired
+    })
+  ).isRequired,
+  row_data: PropTypes.arrayOf(
+    PropTypes.shape({
+      lenderName: PropTypes.string,
+      loanType: PropTypes.string,
+      interestRate: PropTypes.number,
+      closingCosts: PropTypes.number,
+      monthlyPayment: PropTypes.number,
+      apr: PropTypes.number
+    })
+  )
 };
 
 export default ResponsiveTable;
--- test/client/ResponsiveTable.js
+++ test/client/ResponsiveTable.js
@@ -10,9 +10,84 @@ import Adapter from "enzyme-adapter-react-16";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("<ResponsiveTable />", () => {
-  it("renders the text ResponsiveTable", () => {
-    const wrapper = mount(<ResponsiveTable />);
-    console.log(wrapper);
-    expect(true).to.equal(true);
+  const columns = [
+    {
+      title: "Lender",
+      key: "lenderName",
+      hideable: false
+    },
+    {
+      title: "Product",
+      key: "loanType",
+      hideable: false
+    },
+    {
+      title: "Rate",
+      key: "interestRate",
+      hideable: false
+    },
+    {
+      title: "Closing Costs",
+      key: "closingCosts",
+      hideable: false
+    },
+    {
+      title: "Monthly Payment",
+      key: "monthlyPayment",
+      hideable: true
+    },
+    {
+      title: "APR",
+      key: "apr",
+      hideable: true
+    }
+  ];
+
+  const entry = {
+    lenderName: "TFB Federal Credit Union",
+    loanType: "30YR Fixed",
+    interestRate: 4.12567889,
+    closingCosts: 10000,
+    monthlyPayment: 1000,
+    apr: 4.25
+  };
+
+  it("renders a table ", () => {
+    const fake_table = { columns, row_data: [] };
+    const wrapper = mount(<ResponsiveTable {...fake_table} />);
+    expect(wrapper.exists("table")).to.equal(true);
+  });
+
+  it("renders columns even with no row data", () => {
+    const fake_table = {
+      columns,
+      row_data: []
+    };
+    const wrapper = mount(<ResponsiveTable {...fake_table} />);
+    expect(wrapper.find("th")).to.have.length(6);
+  });
+
+  it("renders accuratly supplied columns and row data", () => {
+    const fake_table = {
+      columns,
+      row_data: Array.from(new Array(10).fill(entry))
+    };
+    const wrapper = mount(<ResponsiveTable {...fake_table} />);
+    expect(wrapper.find("th")).to.have.length(6);
+    expect(wrapper.find("td")).to.have.length(60);
+  });
+
+  it("limits the length of floats at 3 decimal places", () => {
+    const fake_table = {
+      columns,
+      row_data: Array.from(new Array(10).fill(entry))
+    };
+    const wrapper = mount(<ResponsiveTable {...fake_table} />);
+    expect(
+      wrapper
+        .find("td")
+        .at(2)
+        .text()
+    ).to.have.length(5);
   });
 });
